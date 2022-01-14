a59f208adcf26907772bc82bdd6fb5c051b7bdd2
komuw <komuw05@gmail.com>
2022-01-14 12:53:15 +0300

    m

---
+++ cool.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+#     # Bash 4.4, Zsh
+#     set -euo pipefail
+# else
+#     # Bash 4.3 and older chokes on empty arrays with set -u.
+#     set -eo pipefail
+# fi
+# shopt -s nullglob globstar
+
+
+rm -rf tyk tyk.conf
+rm -rf my_first_api.json upload_api_with_middleware.json
+
+docker ps -aq | xargs docker rm -f
+fuser -k 7391/tcp
+fuser -k 3121/tcp
+
+echo '{
+  "listen_address": "localhost",
+  "listen_port": 7391,
+  "secret": "changeMe",
+  "template_path": "./templates",
+  "enable_http_profiler": true,
+  "storage": {
+    "type": "redis",
+    "host": "localhost",
+    "port": 6379,
+    "username": "",
+    "password": "",
+    "database": 0
+  }
+}' >> tyk.conf
+
+prev_dir=$(pwd)
+cd my_app && go build -o my_app && ./my_app &
+cd "$prev_dir"
+
+docker run -d --rm -p 6379:6379/tcp redis --loglevel verbose
+
+go build -gcflags="all=-N -l" -o tyk
+
+./tyk start --conf=tyk.conf
+
